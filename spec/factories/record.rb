41ef87e77737b154641eefaf94623880381d0a77
telva0527 <telva0527@gmail.com>
2021-09-19 22:44:33 +0900

    record_spec

---
+++ spec/factories/record.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :record do
+    body { Faker::String.random }
+    classroom { '女川小学校1年1組' }
+    date { Faker::Date.between(from: '2021-01-01', to: '2021-09-20') }
+  end
+end
---
+++ spec/models/record_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Record, type: :model do
+  describe 'バリデーション' do
+    it '備考は必須であること' do
+      record = build(:record, body: nil)
+      record.valid?
+      expect(record.errors[:body]).to include('を入力してください')
+    end
+
+    it '学級は必須であること' do
+      record = build(:record, classroom: nil)
+      record.valid?
+      expect(record.errors[:classroom]).to include('を入力してください')
+    end
+
+    it '日にちは必須であること' do
+      record = build(:record, date: nil)
+      record.valid?
+      expect(record.errors[:date]).to include('を入力してください')
+    end
+  end
+end
