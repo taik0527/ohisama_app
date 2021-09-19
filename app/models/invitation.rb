b724677cc265af829b0542ecabf3e94831f05993
telva0527 <telva0527@gmail.com>
2021-09-19 22:18:21 +0900

    invitation_spec

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,3 +4,4 @@ class Invitation < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :token, presence: true, uniqueness: true
   validates :expired_at, presence: true
+end
---
+++ spec/factories/invitation.rb
@@ -0,0 +1,9 @@
+require 'securerandom'
+
+FactoryBot.define do
+  factory :invitation do
+    email { Faker::Internet.unique.email }
+    token { SecureRandom.hex(10) }
+    expired_at { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+  end
+end
