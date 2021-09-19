7d5317f75bb75a3829d5434565ff24e9bb9f586c
telva0527 <telva0527@gmail.com>
2021-09-19 21:49:46 +0900

    invitation.rb fix

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,4 +3,4 @@
 class Invitation < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :token, presence: true, uniqueness: true
-end
+  validates :expired_at, presence: true
