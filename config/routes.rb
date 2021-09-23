8c134cda9826a3ee73139e61350d03bde4db7040
telva0527 <telva0527@gmail.com>
2021-09-23 21:26:54 +0900

    route.rb fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root to: 'records#index'
   resources :users, only: %i[new create index edit update destroy]
   resources :invitations, only: %i[new create]
